68c1aa74aebf5eb452d1056f99a215585d2c9b14
Viktor Ilin <viktor125142@gmail.com>
2023-12-13 13:53:08 +0300

    fix

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Chashnikov S.Y.
 
 Либо по отдельности pip install transformers pip install sacremoses pip install streamlit pip install torch pip install watchdog - отслеживает изменение в коде и автоматически перезапускает его(Не обязательно ни как не влияет на работу приложения)
 
-запуск приложение можно из терминала командой: streamlit run ilin_vik_web_hw.py
+запуск приложение можно из терминала командой: streamlit run Fast.py
 
 Инструкция Chashnikov.py
 
---
+++ fast.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from transformers import FSMTForConditionalGeneration, FSMTTokenizer
+
+# Загрузка модели и токенизатора
+mname = "facebook/wmt19-ru-en"
+tokenizer = FSMTTokenizer.from_pretrained(mname)
+model = FSMTForConditionalGeneration.from_pretrained(mname)
+
+
+# Определение функции для генерации перевода
+def generate_translation(input_text):
+    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    outputs = model.generate(input_ids)
+    decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return decoded
+
+
+# Веб-приложение с использованием Streamlit
+st.title("Перевод текста с Русского на Английский")
+
+# Получение входного текста от пользователя
+input_text = st.text_input("Введите текст для перевода:", "Это твоя первая программа?")
+
+# Генерация перевода при нажатии кнопки
+if st.button("Перевести"):
+    translation_result = generate_translation(input_text)
+    st.text("Результат перевода:")
+    st.write(translation_result)
